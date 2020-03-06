54f686011afa63708a0338fcbbfc2e08c8416240
Carlos Valdes <carlosv@simplemoviles.com>
2020-03-06 12:09:29 -0300

    initial commit for this learning project

---
+++ leads/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+
+from rest_framework import serializers
+from .models import Lead
+
+
+class LeadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lead
+        fields = ('id', 'name', 'email', 'message')
